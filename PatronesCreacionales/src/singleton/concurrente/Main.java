bc14956d432d657eb302753438929448d9fa64f3
Nidia Ardila Piñeros <nidiaardila17@gmail.com>
2024-10-24 17:37:26 -0300

    Patrones Creacionales - Singleton concurrente. Se utiliza la palabra resevada de Java Sinchronized para garantizar que solo se cree un objeto incluso trabajando de forma concurrente con hilos.

---
+++ PatronesCreacionales/src/singleton/concurrente/Main.java
@@ -0,0 +1,26 @@
+package singleton.concurrente;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				ConexionDB conexion = ConexionDB.obtenerConexion();
+				System.out.println(conexion.hashCode());
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				ConexionDB conexion = ConexionDB.obtenerConexion();
+				System.out.println(conexion.hashCode());
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+	}
+
+}
